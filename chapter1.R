1780aa3d1ddbd499bbcd2e54f6bf5329f1bfa198
Reed Foster <133254542+Dusk1087@users.noreply.github.com>
2025-03-10 19:32:34 -0400

    initial commit

---
+++ chapter1.R
@@ -0,0 +1,25 @@
+wage <- ISLR2::Wage
+auto <- ISLR2::Auto
+smarket <- ISLR2::Smarket
+nci60 <- ISLR2::NCI60
+
+ggplot(wage, aes(age, wage)) +
+  geom_point() +
+  geom_smooth(method = "loess", se = F)
+
+
+ggplot(wage, aes(year, wage)) +
+  geom_point() +
+  geom_smooth(method = "lm")
+
+ggplot(wage, aes(education, wage)) +
+  geom_boxplot(aes(fill = education)) +
+  geom_smooth(method = "lm")
+
+head(smarket)
+dim(smarket)
+
+plot(smarket$Direction, smarket$Lag1)
+
+ggplot(smarket, aes(Direction, Lag1)) +
+  geom_boxplot(aes(fill = Direction))
